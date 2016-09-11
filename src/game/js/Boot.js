03062a95760d641b5bdb9b70f6f021fc61421a7c
AlexeyBerezutsky <alexeyberezutsky@gmail.com>
2016-09-11 17:47:13 +0300

    added platforms

--- src/game/js/Boot.js
+++ src/game/js/Boot.js
@@ -1,45 +1,44 @@
-var Boot = function(game) {
-	this.init = function() {
+var Boot =( function() {
+	return function (game) {
+		this.init = function () {
+			// Unless you specifically know your game needs to support multi-touch I would recommend setting this to 1
+			game.input.maxPointers = 1;
 
-		// Unless you specifically know your game needs to support multi-touch I would recommend setting this to 1
-		this.input.maxPointers = 1;
+			// Pause if the browser tab or game is in loses focus.
+			game.stage.disableVisibilityChange = true;
 
-		// Pause if the browser tab or game is in loses focus.
-		this.stage.disableVisibilityChange = true;
-		
-		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-		
-		// Scale the width down to avoid vertical scrolling
-		this.scale.windowConstraints.bottom = "visual";
-		
-		// Always center the game
-		this.scale.pageAlignHorizontally = true;
-		this.scale.pageAlignVertically = true;
+			game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
-		if (this.game.device.desktop) {
-			
-		} else {
-			// We're saying "scale the game, no lower than 480x260 and no higher than 1024x768"
-			this.scale.setMinMax(480, 260, 1024, 768);
-			this.scale.forceLandscape = true;
-			this.scale.pageAlignHorizontally = true;
-		}
+			// Scale the width down to avoid vertical scrolling
+			game.scale.windowConstraints.bottom = "visual";
 
+			// Always center the game
+			game.scale.pageAlignHorizontally = true;
+			game.scale.pageAlignVertically = true;
+
+			if (game.device.desktop) {
+
+			} else {
+				// We're saying "scale the game, no lower than 480x260 and no higher than 1024x768"
+				game.scale.setMinMax(480, 260, 1024, 768);
+				game.scale.forceLandscape = true;
+				game.scale.pageAlignHorizontally = true;
+			}
+
+		};
+
+		this.preload = function () {
+			// Here we load the assets required for our preloader
+			game.load.image('tile', 'img/tile.png');
+			game.load.image('baddie', 'img/baddie.png');
+			game.load.image('playButton', 'img/lightning-bolt-shadow.png');
+		};
+
+		this.create = function () {
+
+			// By this point the preloader assets have loaded to the cache, we've set the game settings
+			// So now let's start the real preloader going
+			game.state.start('Game');
+		};
 	};
-
-	this.preload = function() {
-
-		// Here we load the assets required for our preloader
-		//this.load.image('preloaderBackground', 'images/preloader_background.jpg');
-		//this.load.image('preloaderBar', 'images/preloadr_bar.png');
-
-	};
-
-	this.create = function() {
-
-		// By this point the preloader assets have loaded to the cache, we've set the game settings
-		// So now let's start the real preloader going
-		this.state.start('Preloader');
-
-	};
-}
+})();
--- src/game/js/Game.js
+++ src/game/js/Game.js
@@ -1,41 +1,112 @@
-var Game = function (game) {
-    //  When a State is added to Phaser it automatically has the following properties set on it, even if they already exist:
+var Game = (function(){
+    return function (game) {
+        //  When a State is added to Phaser it automatically has the following properties set on it, even if they already exist:
 
-    var add;       //  used to add sprites, text, groups, etc (Phaser.GameObjectFactory)
-    var camera;    //  a reference to the game camera (Phaser.Camera)
-    var cache;     //  the game cache (Phaser.Cache)
-    var input;     //  the global input manager. You can access this.input.keyboard, this.input.mouse, as well from it. (Phaser.Input)
-    var load;      //  for preloading assets (Phaser.Loader)
-    var math;      //  lots of useful common math operations (Phaser.Math)
-    var sound;     //  the sound manager - add a sound, play one, set-up markers, etc (Phaser.SoundManager)
-    var stage;     //  the game stage (Phaser.Stage)
-    var time;      //  the clock (Phaser.Time)
-    var tweens;    //  the tween manager (Phaser.TweenManager)
-    var state;     //  the state manager (Phaser.StateManager)
-    var world;     //  the game world (Phaser.World)
-    var particles; //  the particle manager (Phaser.Particles)
-    var physics;   //  the physics manager (Phaser.Physics)
-    var rnd;       //  the repeatable random number generator (Phaser.RandomDataGenerator)
-	
-	this.init = function(){
-		// Create simple text display for current Phaser version
-        var text = "Phaser works!";
-        var style = { font: "24px Arial", fill: "#fff", align: "center" };
-        var t = game.add.text(this.world.centerX, this.world.centerY, text, style);
-        t.anchor.setTo(0.5, 0.5);
-	}
-	
-	this.create = function () {
+        var add;       //  used to add sprites, text, groups, etc (Phaser.GameObjectFactory)
+        var camera;    //  a reference to the game camera (Phaser.Camera)
+        var cache;     //  the game cache (Phaser.Cache)
+        var input;     //  the global input manager. You can access this.input.keyboard, this.input.mouse, as well from it. (Phaser.Input)
+        var load;      //  for preloading assets (Phaser.Loader)
+        var math;      //  lots of useful common math operations (Phaser.Math)
+        var sound;     //  the sound manager - add a sound, play one, set-up markers, etc (Phaser.SoundManager)
+        var stage;     //  the game stage (Phaser.Stage)
+        var time;      //  the clock (Phaser.Time)
+        var tweens;    //  the tween manager (Phaser.TweenManager)
+        var state;     //  the state manager (Phaser.StateManager)
+        var world;     //  the game world (Phaser.World)
+        var particles; //  the particle manager (Phaser.Particles)
+        var physics;   //  the physics manager (Phaser.Physics)
+        var rnd;       //  the repeatable random number generator (Phaser.RandomDataGenerator)
+        var tileWidth;
+        var tileHeight;
 
-    };
+        this.init = function () {
 
-    this.update = function () {
+            //Get the dimensions of the tile we are using
+            tileWidth = game.cache.getImage('tile').width;
+            tileHeight = game.cache.getImage('tile').height;
 
-    };
+            //Set the background colour to blue
+            game.stage.backgroundColor = '479cde';
 
-    this.quitGame = function (pointer) {
+            //Enable the Arcade physics system
+            game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        this.state.start('MainMenu');
-    };
+        };
 
-};
+        this.create = function () {
+            //Add a platforms group to hold all of our tiles, and create a bunch of them
+            game.platforms = game.add.group();
+            game.platforms.enableBody = true;
+            game.platforms.createMultiple(250, 'tile');
+
+            //The spacing for the initial platforms
+            game.spacing = 300;
+
+//Create the inital on screen platforms
+            initPlatforms();
+
+            var timer = game.time.events.loop(2000, addPlatform, this);
+        };
+
+
+        var addTile = function(x, y){
+
+            //Get a tile that is not currently on screen
+            var tile = game.platforms.getFirstDead();
+
+            //Reset it to the specified coordinates
+            tile.reset(x, y);
+            tile.body.velocity.y = 150;
+            tile.body.immovable = true;
+
+            //When the tile leaves the screen, kill it
+            tile.checkWorldBounds = true;
+            tile.outOfBoundsKill = true;
+        };
+
+        var addPlatform = function(y){
+
+            var me = this;
+
+            //If no y position is supplied, render it just outside of the screen
+            if(typeof(y) == "undefined"){
+                y = -tileHeight;
+            }
+
+            //Work out how many tiles we need to fit across the whole screen
+            var tilesNeeded = Math.ceil(game.world.width / tileWidth);
+
+            //Add a hole randomly somewhere
+            var hole = Math.floor(Math.random() * (tilesNeeded - 3)) + 1;
+
+            //Keep creating tiles next to each other until we have an entire row
+            //Don't add tiles where the random hole is
+            for (var i = 0; i < tilesNeeded; i++){
+                if (i != hole && i != hole + 1){
+                    addTile(i * tileWidth, y);
+                }
+            }
+
+        };
+
+        var initPlatforms =function(){
+
+            var me = this,
+                bottom = game.world.height - tileHeight,
+                top = tileHeight;
+
+            //Keep creating platforms until they reach (near) the top of the screen
+            for(var y = bottom; y > top - tileHeight; y = y - game.spacing){
+                addPlatform(y);
+            }
+
+        };
+
+        this.update = function () {
+
+        };
+
+    }
+
+})();
