77766635e8fb86a838e6eb1c4014b9a630804512
Alexey Berezutsky <alexey.berezutsky@sidenis.com>
2016-09-16 15:50:38 +0300

    reformatted code

--- src/game/js/Boot.js
+++ src/game/js/Boot.js
@@ -14,32 +14,28 @@ var Boot =( function() {
 
 			// Always center the game
 			game.scale.pageAlignHorizontally = true;
+
 			game.scale.pageAlignVertically = true;
 
 			if (game.device.desktop) {
-
 			} else {
 				// We're saying "scale the game, no lower than 480x260 and no higher than 1024x768"
 				game.scale.setMinMax(480, 260, 1024, 768);
+
 				game.scale.forceLandscape = true;
+
 				game.scale.pageAlignHorizontally = true;
 			}
-
-
 		};
-
-
 
 		this.preload = function () {
 			// Here we load the assets required for our preloader
 			game.load.image('tile', 'img/tile.png');
+
 			game.load.spritesheet('baddie','img/baddie.png', 32, 32, 4);
 		};
 
 		this.create = function () {
-
-			// By this point the preloader assets have loaded to the cache, we've set the game settings
-			// So now let's start the real preloader going
 			game.state.start('Game');
 		};
 	};
--- src/game/js/Game.js
+++ src/game/js/Game.js
@@ -2,43 +2,57 @@ var Game = (function () {
     return function (game) {
         //  When a State is added to Phaser it automatically has the following properties set on it, even if they already exist:
 
-        var add;       //  used to add sprites, text, groups, etc (Phaser.GameObjectFactory)
-        var camera;    //  a reference to the game camera (Phaser.Camera)
-        var cache;     //  the game cache (Phaser.Cache)
-        var input;     //  the global input manager. You can access this.input.keyboard, this.input.mouse, as well from it. (Phaser.Input)
-        var load;      //  for preloading assets (Phaser.Loader)
-        var math;      //  lots of useful common math operations (Phaser.Math)
-        var sound;     //  the sound manager - add a sound, play one, set-up markers, etc (Phaser.SoundManager)
-        var stage;     //  the game stage (Phaser.Stage)
-        var time;      //  the clock (Phaser.Time)
-        var tweens;    //  the tween manager (Phaser.TweenManager)
-        var state;     //  the state manager (Phaser.StateManager)
-        var world;     //  the game world (Phaser.World)
-        var particles; //  the particle manager (Phaser.Particles)
-        var physics;   //  the physics manager (Phaser.Physics)
-        var rnd;       //  the repeatable random number generator (Phaser.RandomDataGenerator)
-        var tileWidth;
-        var tileHeight;
-        var spacing;
-        var player, platforms;
-        var cursors, pad;
-        var score, scoreLabel;
-
         this.init = function () {
-
-            //Get the dimensions of the tile we are using
-            tileWidth = game.cache.getImage('tile').width;
-            tileHeight = game.cache.getImage('tile').height;
-
             //Set the background colour to blue
             game.stage.backgroundColor = '479cde';
 
             //Enable the Arcade physics system
             game.physics.startSystem(Phaser.Physics.ARCADE);
-
         };
 
-        var initPlatforms = function () {
+        this.create = function () {
+            game.source = game.source || {platforms: createPlatforms()};
+
+            initPlatforms();
+
+            runPlatforms(addPlatform);
+
+            createPlayer();
+
+            initControls({
+                pad: game.input.gamepad.pad1,
+
+                keyboard: game.input.keyboard.createCursorKeys()
+            });
+
+            createScore();
+        };
+
+        this.update = function () {
+            checkCollisions();
+
+            checkPlayerPosition();
+
+            handlePlayerAnimation();
+        };
+
+        var createPlatforms = function () {
+            var platforms = game.add.group();
+
+            platforms.enableBody = true;
+
+            //create pool of objects;
+
+            platforms.createMultiple(250, 'tile');
+
+            return platforms;
+        };
+
+        var initPlatforms = function (platforms) {
+            var tileHeight = game.cache.getImage('tile').height;
+
+            var spacing = 3 * tileHeight;
+
             var bottom = game.world.height - tileHeight,
                 top = tileHeight;
 
@@ -49,7 +63,10 @@ var Game = (function () {
         };
 
         var addPlatform = function (y) {
+            var tileWidth = game.cache.getImage('tile').width;
+
             //If no y position is supplied, render it just outside of the screen
+
             if (typeof(y) == "undefined") {
                 y = -tileHeight;
 
@@ -73,7 +90,7 @@ var Game = (function () {
 
         var addTile = function (x, y) {
             //Get a tile that is not currently on screen
-            var tile = platforms.getFirstDead();
+            var tile = game.source.platforms.getFirstDead();
 
             //Reset it to the specified coordinates
             tile.reset(x, y);
@@ -88,9 +105,16 @@ var Game = (function () {
             tile.outOfBoundsKill = true;
         };
 
+        var runPlatforms = function (callBack) {
+            var timer = game.time.events.loop(2000, callBack, this);
+        };
+
+
         var createPlayer = function () {
+            var tileHeight = game.cache.getImage('tile').height;
+
             //Add the player to the game by creating a new sprite
-            player = game.add.sprite(game.world.centerX, game.world.height - (spacing * 2 + (3 * tileHeight)), 'baddie');
+            var player = game.add.sprite(game.world.centerX, game.world.height - 7 * tileHeight, 'baddie');
 
             player.scale.set(2);
 
@@ -105,7 +129,6 @@ var Game = (function () {
             //Make the player fall by applying gravity
             player.body.gravity.y = 2000;
 
-
             //Make the player collide with the game boundaries
             player.body.collideWorldBounds = true;
 
@@ -116,88 +139,68 @@ var Game = (function () {
 
             player.animations.add('right', [2, 3], 10, true);
 
-            player.body.bounce.set(0.4);
+            player.body.bounce.set(0.1);
+
+            game.source.player = player;
+        };
+
+        var initControls = function (controlls) {
+            initPad(game.controlls.pad);
+
+            initKeyBoard(game.controlls.keyBoard);
+        };
+
+        var initPad = function () {
+            game.input.gamepad.start();
+
+            var pad = game.input.gamePad.pad1;
+
+            if (pad) {
+                pad.addCallbacks(this, {onConnect: addButtons});
+            } else {
+                console.log('Gamepad is not supported');
+            }
+        };
+
+        var addButtons = function () {
+            var pad = game.input.gamePad.pad1;
+
+            var rightTriggerButton = pad.getButton(Phaser.Gamepad.XBOX360_RIGHT_TRIGGER);
+
+            rightTriggerButton.onFloatCallback(onRightTrigger);
+
+            var leftStick = pad.getButton(Phaser.Gamepad.XBOX360_RIGHT_TRIGGER);
+
+            leftStick.onAxisCallback(onStickAxisChanged);
         };
 
         var onRightTrigger = function (button, value) {
-            console.log(value);
             if (player.body.wasTouching.down) {
+
                 player.body.velocity.y = -value * 2000;
             }
         };
 
-        function addButtons() {
-            var pad = game.input.gamepad.pad1;
-
-            var rightTriggerButton = pad.getButton(Phaser.Gamepad.XBOX360_RIGHT_TRIGGER);
-
-            rightTriggerButton.onFloat.add(onRightTrigger);
-        }
-
-        var createScore = function(){
-            var scoreFont = "100px Arial";
-
-            scoreLabel = game.add.text((game.world.centerX), 100, "0", {font: scoreFont, fill: "#fff"});
-            scoreLabel.anchor.setTo(0.5, 0.5);
-            scoreLabel.align = 'center';
-        };
-
-        var incrementScore = function(){
-            score += 1;
-            scoreLabel.text = score;
-        };
-
-        this.create = function () {
-            //Add a platforms group to hold all of our tiles, and create a bunch of them
-            platforms = game.add.group();
-
-            platforms.enableBody = true;
-
-            platforms.createMultiple(250, 'tile');
-
-            //The spacing for the initial platforms
-            spacing = 200;
-
-            //Create the inital on screen platforms
-
-            initPlatforms();
-
-            createPlayer();
-
-            score = 0;
-
-            createScore();
-
-            game.input.gamepad.start();
-
-            // if (game.input.gamepad.supported && game.input.gamepad.active && game.input.gamepad.pad1.connected) {
-            pad = game.input.gamepad.pad1;
-
-            pad.addCallbacks(this, {onConnect: addButtons});
-            //} else {
-            cursors = game.input.keyboard.createCursorKeys();
-            //}
-
-
-
-            var timer = game.time.events.loop(2000, addPlatform, this);
-        };
-
-        var gameOver = function () {
-            game.state.start('Game');
-        };
-        var animationIsOn = false;
-
-
-        this.update = function () {
-            //Make the sprite collide with the ground layer
-            game.physics.arcade.collide(player, platforms);
-
-            //Check if the player is touching the bottom
-            if (player.body.position.y >= game.world.height - player.body.height) {
-                gameOver();
+        var onStickAxisChanged = function () {
+            if (pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1) {
+                player.body.velocity.x += pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) * 10;
             }
+            else if (pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1) {
+                player.body.velocity.x += pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) * 10;
+            }
+        };
 
+        var initKeyBoard = function () {
+            var cursors = game.input.keyboard.createCursorKeys();
+
+            var keyboard = new Keyboard(game);
+
+            keyboard.onDownCallback(onDownKeyboard);
+
+
+        };
+
+        var onDownKeyboard = function (button) {
             if (cursors) {
                 //Make the sprite jump when the up key is pushed
                 if (cursors.up.isDown && player.body.wasTouching.down) {
@@ -210,31 +213,62 @@ var Game = (function () {
                 } else if (cursors.right.isDown) {
                     player.body.velocity.x += 50;
                 } else {
-                  animationIsOn = false;
+                    animationIsOn = false;
                 }
             }
+        };
 
-            if (game.input.gamepad.supported && game.input.gamepad.active && game.input.gamepad.pad1.connected) {
-                if (pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1) {
-                    player.body.velocity.x += pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) * 10;
-                }
-            else if (pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1) {
-                player.body.velocity.x += pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) * 10;
+        var createScore = function () {
+            game.score = 0;
+
+            var scoreFont = "100px Arial";
+
+            game.scoreLabel = game.add.text((game.world.centerX), 100, "0", {font: scoreFont, fill: "#fff"});
+
+            game.scoreLabel.anchor.setTo(0.5, 0.5);
+
+            game.scoreLabel.align = 'center';
+        };
+
+        var incrementScore = function () {
+            game.score += 1;
+
+            game.scoreLabel.text = score;
+        };
+
+
+
+        var gameOver = function () {
+            game.state.start('Game');
+        };
+
+        var checkCollisions = function(){
+            var player = game.source.player;
+
+            //Make the sprite collide with the ground layer
+            game.physics.arcade.collide(player, game.source.platforms);
+        };
+
+        var checkPlayerPosition = function(){
+            //Check if the player is touching the bottom
+            if (player.body.position.y >= game.world.height - player.body.height) {
+                gameOver();
+            }
+        };
+
+        var handlePlayerAnimation = function(){
+            if (player.body.velocity.x > 0.1) {
+                player.animations.play('right');
+                player.body.velocity.x -= 1;
+            } else if (player.body.velocity.x < -0.1) {
+                player.body.velocity.x += 1;
+                player.animations.play('left');
+            } else {
+                player.animations.stop();
             }
         };
 
 
-        if (player.body.velocity.x> 0.1) {
-            player.animations.play('right');
-            player.body.velocity.x -= 1;
-        } else if(player.body.velocity.x < -0.1){
-            player.body.velocity.x += 1;
-            player.animations.play('left');
-        } else {
-            player.animations.stop();
-        }
-
-    };
-}
+    }
 })
 ();
