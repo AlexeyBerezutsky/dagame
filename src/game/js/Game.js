d5f8fdaa42e357b5c9996510bf8e08c502306f4d
Alexey Berezutsky <alexey.berezutsky@sidenis.com>
2016-09-19 18:01:42 +0300

    reformatted code

--- src/game/js/Game.js
+++ src/game/js/Game.js
@@ -2,26 +2,62 @@ var Game = (function () {
     return function (game) {
         //  When a State is added to Phaser it automatically has the following properties set on it, even if they already exist:
 
+        var player, platforms, bricks, bullets;
+
+        var input = {};
+
+        var animationRun;
+
+        var hitchSize;
+
+        var timer;
+
+        var constants = {
+            PLATFORM_SPEED: 80,
+
+            PLATFORM_CREATE_FREQ: 2000,
+
+            USER_GRAVITY: 2000,
+
+            USER_BOUNCE: 0.1,
+
+            SHOT_DELAY: 10,
+
+            BULLET_SPEED: 700,
+
+            NUMBER_OF_BULLETS: 20,
+
+            BULLET_GRAVITY: 500,
+
+            BULLET_BOUNCE: 0.8
+        };
+
+        var score = {
+            value: 0,
+
+            label: ''
+        };
+
         this.init = function () {
             //Set the background colour to blue
             game.stage.backgroundColor = '479cde';
 
             //Enable the Arcade physics system
             game.physics.startSystem(Phaser.Physics.ARCADE);
+
+            hitchSize = game.cache.getImage('tile').height;
         };
 
         this.create = function () {
-            game.source = game.source || {};
-
             createHitches();
 
             initPlatforms();
 
-            game.source.timer = runPlatforms(addPlatform);
+            timer = runPlatforms(addPlatform);
 
-            createBullets();
+            bullets = createBullets();
 
-            createPlayer();
+            player = createPlayer();
 
             initControls();
 
@@ -36,13 +72,13 @@ var Game = (function () {
             handlePlayerAnimation();
         };
 
-        var createHitches = function(){
-            game.source.platforms = createPlatforms();
+        var createHitches = function () {
+            platforms = createPlatforms();
 
-            game.source.bricks = createBricks();
+            bricks = createBricks();
         };
 
-        var createBricks = function(){
+        var createBricks = function () {
             var bricks = game.add.group();
 
             bricks.enableBody = true;
@@ -54,9 +90,7 @@ var Game = (function () {
             return bricks;
         };
 
-        var destroyBrick = function(brick, bullet){
-            incrementScore(10);
-
+        var destroyBrick = function (brick, bullet) {
             brick.kill();
 
             bullet.kill();
@@ -77,34 +111,28 @@ var Game = (function () {
         };
 
         var initPlatforms = function () {
-            var tileHeight = game.cache.getImage('tile').height;
+            var spacing = 3 * hitchSize;
 
-            var spacing = 3 * tileHeight;
-
-            var bottom = game.world.height - tileHeight,
-                top = tileHeight;
+            var bottom = game.world.height - hitchSize,
+                top = hitchSize;
 
             //Keep creating platforms until they reach (near) the top of the screen
-            for (var y = bottom; y > top - tileHeight; y = y - spacing) {
+            for (var y = bottom; y > top - hitchSize; y = y - spacing) {
                 addPlatform(y);
             }
         };
 
         var addPlatform = function (y) {
-            var tileWidth = game.cache.getImage('tile').width;
-
-            var tileHeight = game.cache.getImage('tile').height;
-
             //If no y position is supplied, render it just outside of the screen
 
             if (typeof(y) == "undefined") {
-                y = -tileHeight;
+                y = -hitchSize;
 
                 incrementScore();
             }
 
             //Work out how many tiles we need to fit across the whole screen
-            var tilesNeeded = Math.ceil(game.world.width / tileWidth);
+            var tilesNeeded = Math.ceil(game.world.width / hitchSize);
 
             //Add a hole randomly somewhere
             var hole = Math.floor(Math.random() * (tilesNeeded - 3)) + 1;
@@ -113,10 +141,10 @@ var Game = (function () {
             //Don't add tiles where the random hole is
             for (var i = -1; i <= tilesNeeded; i++) {
                 if (i != hole && i != hole + 1) {
-                    addTile(i * tileWidth, y);
+                    addTile(i * hitchSize, y);
                 }
-                else if(Math.floor(Math.random() * 10) + 1 > 8  ){
-                    addBrick(i * tileWidth, y);
+                else if (Math.floor(Math.random() * 10) + 1 > 8) {
+                    addBrick(i * hitchSize, y);
                 }
 
             }
@@ -124,12 +152,12 @@ var Game = (function () {
 
         var addTile = function (x, y) {
             //Get a tile that is not currently on screen
-            var tile = game.source.platforms.getFirstDead();
+            var tile = platforms.getFirstDead();
 
             //Reset it to the specified coordinates
             tile.reset(x, y);
 
-            tile.body.velocity.y = 80;
+            tile.body.velocity.y = constants.PLATFORM_SPEED;
 
             tile.body.immovable = true;
 
@@ -139,14 +167,14 @@ var Game = (function () {
             tile.outOfBoundsKill = true;
         };
 
-        var addBrick = function(x,y){
+        var addBrick = function (x, y) {
             //Get a tile that is not currently on screen
-            var brick = game.source.bricks.getFirstDead();
+            var brick = bricks.getFirstDead();
 
             //Reset it to the specified coordinates
             brick.reset(x, y);
 
-            brick.body.velocity.y = 80;
+            brick.body.velocity.y = constants.PLATFORM_SPEED;
 
             brick.body.immovable = true;
 
@@ -157,14 +185,12 @@ var Game = (function () {
         };
 
         var runPlatforms = function (callBack) {
-            return game.time.events.loop(3000, callBack, this);
+            return game.time.events.loop(constants.PLATFORM_CREATE_FREQ, callBack, this);
         };
 
         var createPlayer = function () {
-            var tileHeight = game.cache.getImage('tile').height;
-
             //Add the player to the game by creating a new sprite
-            var player = game.add.sprite(game.world.centerX, game.world.height - 7 * tileHeight, 'baddie');
+            var player = game.add.sprite(game.world.centerX, game.world.height - 7 * hitchSize, 'baddie');
 
             player.scale.set(2);
 
@@ -177,7 +203,7 @@ var Game = (function () {
             game.physics.arcade.enable(player);
 
             //Make the player fall by applying gravity
-            player.body.gravity.y = 2000;
+            player.body.gravity.y = constants.USER_GRAVITY;
 
             //Make the player collide with the game boundaries
             player.body.collideWorldBounds = true;
@@ -191,20 +217,23 @@ var Game = (function () {
 
             player.frame = 1;
 
-            player.body.bounce.set(0.1);
+            player.body.bounce.set(constants.USER_BOUNCE);
 
-            game.source.player = player;
+            return player;
         };
 
         var initControls = function () {
-            initPad();
+            input.pad = initPad();
 
-            initKeyBoard();
+            input.keyboard = initKeyBoard();
         };
 
         var initPad = function () {
-            game.input.gamepad.start();
-            if (game.input.gamepad.supported && game.input.gamepad.active) {
+            var gamepad = game.input.gamepad;
+
+            gamepad.start();
+
+            if (gamepad.supported && gamepad.active) {
                 var pad = game.input.gamepad.pad1;
 
                 if (pad) {
@@ -212,50 +241,46 @@ var Game = (function () {
                 } else {
                     console.log('can not find first gamepad');
                 }
+                return pad;
             } else {
                 console.log('Gamepad is not supported');
             }
         };
 
         var addButtons = function () {
-            var pad = game.input.gamepad.pad1;
-
-            var rightTriggerButton = pad.getButton(Phaser.Gamepad.XBOX360_RIGHT_TRIGGER);
+            var rightTriggerButton = input.pad.getButton(Phaser.Gamepad.XBOX360_RIGHT_TRIGGER);
 
             rightTriggerButton.onFloat.add(onRightTrigger);
 
-            pad.addCallbacks(this, {
+            input.pad.addCallbacks(this, {
                 onAxis: onStickAxisChanged
             });
 
-            var AButton = pad.getButton(Phaser.Gamepad.XBOX360_A);
+            var AButton = input.pad.getButton(Phaser.Gamepad.XBOX360_A);
 
             AButton.onDown.add(shootBullet);
         };
 
         var onRightTrigger = function (button, value) {
-            var player = game.source.player;
             if (player.body.wasTouching.down) {
-                game.source.animationRun = false;
+                animationRun = false;
 
                 player.body.velocity.y = -value * 3000;
             }
         };
 
         var onStickAxisChanged = function (pad, axis, value) {
-            var player = game.source.player;
-
             if (value < -0.1) {
-                game.source.animationRun = true;
+                animationRun = true;
 
                 player.body.velocity.x = value * 500;
             }
             else if (value > 0.1) {
-                game.source.animationRun = true;
+                animationRun = true;
 
                 player.body.velocity.x = value * 500;
             } else {
-                game.source.animationRun = false;
+                animationRun = false;
             }
         };
 
@@ -263,16 +288,17 @@ var Game = (function () {
             var keyboard = game.input.keyboard;
 
             keyboard.onDownCallback = onDownKeyboard;
+
             keyboard.onUpCallback = onUpKeyboard;
+
+            return keyboard;
         };
 
         var onDownKeyboard = function (button) {
-            var player = game.source.player;
-
             switch (button.keyCode) {
                 case Phaser.KeyCode.UP:
                     if (player.body.wasTouching.down) {
-                        game.source.animationRun = false;
+                        animationRun = false;
 
                         player.body.velocity.y = -1400;
                     }
@@ -280,14 +306,14 @@ var Game = (function () {
                     break;
 
                 case Phaser.KeyCode.RIGHT:
-                    game.source.animationRun = true;
+                    animationRun = true;
 
                     player.body.velocity.x += 50;
 
                     break;
 
                 case Phaser.KeyCode.LEFT:
-                    game.source.animationRun = true;
+                    animationRun = true;
 
                     player.body.velocity.x -= 50;
 
@@ -298,56 +324,48 @@ var Game = (function () {
 
                     break;
             }
-
-            console.log(player.body.velocity.x);
         };
 
         var onUpKeyboard = function (button) {
             if ([Phaser.KeyCode.RIGHT, Phaser.KeyCode.LEFT].indexOf(button.keyCode) !== -1) {
-                game.source.animationRun = false;
+                animationRun = false;
             }
         };
 
         var createScore = function () {
-            game.source.score = 0;
-
             var scoreFont = "100px Arial";
 
-            game.source.scoreLabel = game.add.text((game.world.centerX), 100, "0", {font: scoreFont, fill: "#fff"});
+            score.label = game.add.text((game.world.centerX), 100, "0", {font: scoreFont, fill: "#fff"});
 
-            game.source.scoreLabel.anchor.setTo(0.5, 0.5);
+            score.label.anchor.setTo(0.5, 0.5);
 
-            game.source.scoreLabel.align = 'center';
+            score.label.align = 'center';
         };
 
-        var incrementScore = function (increment) {
-            if (!increment){
-                increment = 1;
-            }
-            game.source.score += increment;
+        var incrementScore = function () {
+            score.value += 1;
 
-            game.source.scoreLabel.text = game.source.score;
+            score.label.text = score.value;
         };
 
         var gameOver = function () {
-            game.source.timer.timer.removeAll();
+            timer.timer.removeAll();
 
             game.state.start('Game');
         };
 
         var checkCollisions = function () {
             //Make the sprite collide with the ground layer
-            game.physics.arcade.collide(game.source.player, game.source.platforms);
+            game.physics.arcade.collide(player, platforms);
 
-            game.physics.arcade.collide(game.source.platforms, game.source.bullets);
+            game.physics.arcade.collide(platforms, bullets);
 
-            game.physics.arcade.collide(game.source.bricks, game.source.player);
+            game.physics.arcade.collide(bricks, player);
 
-            game.physics.arcade.overlap(game.source.bricks, game.source.bullets, destroyBrick, null, game);
+            game.physics.arcade.overlap(bricks, bullets, destroyBrick, null, game);
         };
 
         var checkPlayerPosition = function () {
-            var player = game.source.player;
             //Check if the player is touching the bottom
             if (player.body.position.y >= game.world.height - player.body.height) {
                 gameOver();
@@ -355,9 +373,7 @@ var Game = (function () {
         };
 
         var handlePlayerAnimation = function () {
-            var player = game.source.player;
-
-            if (!game.source.animationRun) {
+            if (!animationRun) {
                 player.animations.stop();
 
                 if (player.body.velocity.x > 0.01) {
@@ -377,18 +393,10 @@ var Game = (function () {
             }
         };
 
-       var createBullets = function() {
-           game.source.SHOT_DELAY = 10; // milliseconds (10 bullets/second)
+        var createBullets = function () {
+            var bullets = game.add.group();
 
-           game.source.BULLET_SPEED = 700; // pixels/second
-
-           game.source.NUMBER_OF_BULLETS = 20;
-
-            // Create an object pool of bullets
-
-           var bullets = game.add.group();
-
-            for(var i = 0; i <  game.source.NUMBER_OF_BULLETS; i++) {
+            for (var i = 0; i < constants.NUMBER_OF_BULLETS; i++) {
                 // Create each bullet and add it to the group.
                 var bullet = game.add.sprite(0, 0, 'fireball');
 
@@ -403,35 +411,39 @@ var Game = (function () {
                 game.physics.arcade.enable(bullet);
 
                 //Make the player fall by applying gravity
-                bullet.body.gravity.y = 500;
+                bullet.body.gravity.y = constants.BULLET_GRAVITY;
 
                 //Make the player collide with the game boundaries
                 bullet.body.collideWorldBounds = true;
 
-                bullet.body.bounce.set(0.8);
+                bullet.body.bounce.set(constants.BULLET_BOUNCE);
 
                 // Set its initial state to "dead".
                 bullet.kill();
             }
 
-           game.source.bullets = bullets;
+            return bullets;
         };
 
-        var shootBullet = function() {
+        var shootBullet = function () {
             // Enforce a short delay between shots by recording
             // the time that each bullet is shot and testing if
             // the amount of time since the last shot is more than
             // the required delay.
             var lastBulletShotAt;
 
-            if (lastBulletShotAt === undefined) {lastBulletShotAt = 0;}
+            if (lastBulletShotAt === undefined) {
+                lastBulletShotAt = 0;
+            }
 
-            if (game.time.now - lastBulletShotAt < game.source.SHOT_DELAY) {return;}
+            if (game.time.now - lastBulletShotAt < constants.SHOT_DELAY) {
+                return;
+            }
 
             lastBulletShotAt = game.time.now;
 
             // Get a dead bullet from the pool
-            var bullet = game.source.bullets.getFirstDead();
+            var bullet = bullets.getFirstDead();
 
             // If there aren't any bullets available then don't shoot
             if (bullet === null || bullet === undefined) return;
@@ -449,12 +461,12 @@ var Game = (function () {
             bullet.outOfBoundsKill = true;
 
             // Set the bullet position to the gun position.
-            bullet.reset(game.source.player.x, game.source.player.y - game.cache.getImage('player').height/2);
+            bullet.reset(player.x, player.y - hitchSize / 2);
 
             // Shoot it
             bullet.body.velocity.x = 0;
 
-            bullet.body.velocity.y = -game.source.BULLET_SPEED;
+            bullet.body.velocity.y = -constants.BULLET_SPEED;
         };
     }
 })();
